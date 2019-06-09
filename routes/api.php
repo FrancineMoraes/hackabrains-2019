2432bf7e5a418dc3c897aa1dc685c9ab56ecab05
Francine <francinemoraes08@hotmail.com>
2019-06-09 00:45:57 -0300

    api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('login', 'Auth\LoginController@login');
+//Route::post('', 'Auth\LoginController@login');
+
+Route::group(['middleware' => ['web']], function () {
+    // your routes here
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
