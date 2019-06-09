2c561c1e79f384026b3a554b434939868d7ac666
unknown <caiozambonatoferreira@gmail.com>
2019-06-09 00:17:04 -0300

    Init map

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import './registerServiceWorker'
 import 'buefy/dist/buefy.css'
 import Buefy from 'buefy'
 import VueSweetalert2 from 'vue-sweetalert2'
- 
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 Vue.use(VueSweetalert2)
 Vue.use(Buefy)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCVlTWn7lC5WqRlyz82YaJZrYE9A-CJV4A',
+    libraries: 'places', 
+	}
+})
 
 Vue.config.productionTip = false
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Main from './views/Main.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
+    },
+
+    {
+      path: '/main',
+      name: 'Main',
+      component: Main
     }
   ]
 })
